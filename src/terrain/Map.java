1453a01d3a347760a5b5bda2f0601f3e53253980
kylehhui <kylehhui@gmail.com>
2026-05-05 20:35:07 -0700

    Screen Scrolling Improved

--- src/terrain/Map.java
+++ src/terrain/Map.java
@@ -4,12 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
+import java.util.ArrayList;
+
 public class Map extends JPanel {
     // boolean used later on for tracking if the mouse is pressed
     public static boolean pressed = false;
     // numbers to track screen translation
     public static int deltaX = 0;
     public static int deltaY = 0;
+
+    // boolean array to check if each arrow key is pressed
+    // indexes of boolean array
+    // 0 corresponds to left
+    // 1 corresponds to right
+    // 2 corresponds to up
+    // 3 corresponds to down
+    public static boolean[] keysPressed = {false, false, false, false};
+
+    // each keyMovement corresponds to a translation
+    // ie index 0 being pressed will signify keycode 37 (left arrow) is being pressed, translating x to the left
+    public static final int[] keyMovements = {10,-10,10,-10};
 
     @Override
     protected void paintComponent(Graphics g)
@@ -68,12 +82,7 @@ public class Map extends JPanel {
             @Override
             public void mousePressed(MouseEvent e)
             {       
-                //TEST
-                deltaY += 100;
-                frame.repaint();
-                
                 pressed = true;
-                System.out.println(pressed);
             }
         });
         // detect when mouse is released
@@ -83,21 +92,92 @@ public class Map extends JPanel {
             public void mouseReleased(MouseEvent e)
             {       
                 pressed = false;
-                System.out.println(pressed);
             }
         });
 
         // detect arrow key presses for screen translation
+        // 37 = left arrow
+        // 38 = up arrow
+        // 39 = right arrow
+        // 40 = down arrow
+
         frame.addKeyListener(new KeyListener() 
         {
             @Override
             public void keyPressed(KeyEvent e)
             {
-                System.out.println(e);
+                // update keysPressed array to reflect which keys are currently pressed
+                if (e.getKeyCode() == 37)
+                {
+                    keysPressed[0] = true;
+                }
+                if (e.getKeyCode() == 39)
+                {
+                    keysPressed[1] = true;
+                }
+                if (e.getKeyCode() == 38)
+                {
+                    keysPressed[2] = true;
+                }
+                if (e.getKeyCode() == 40)
+                {
+                    keysPressed[3] = true;
+                }
+
+                // movement: checks if any keys were pressed by referencing the keysPressed array
+                ArrayList<Integer> movements = new ArrayList<Integer>();
+                // loops through each key to see if it is pressed
+                for (int i = 0; i < 4; i ++)
+                {
+                    if (keysPressed[i])
+                    {
+                        movements.add(i);
+                    }
+                }
+                
+                // if any movement keys are pressed
+                if (movements.size() > 0)
+                {
+                    // loop through pressed keys
+                    for (int i = 0; i < movements.size(); i++)
+                    {
+                        int key = movements.get(i);
+                        if (key == 0 || key == 1)
+                        {
+                            deltaX += keyMovements[key];
+                        }
+                        if (key == 2 || key == 3)
+                        {
+                            deltaY += keyMovements[key];
+                        }
+                    }
+                    // repaint canvas with translations
+
+                    frame.repaint();
+                }
+
             }
             public void keyReleased(KeyEvent e)
             {
+                // update keysPressed arrays to reflect which keys are no longer pressed
+                if (e.getKeyCode() == 37)
+                {
+                    keysPressed[0] = false;
+                }
+                if (e.getKeyCode() == 39)
+                {
+                    keysPressed[1] = false;
+                }
+                if (e.getKeyCode() == 38)
+                {
+                    keysPressed[2] = false;
+                }
+                if (e.getKeyCode() == 40)
+                {
+                    keysPressed[3] = false;
+                }
             }
+            // all 3 methods must be implemented even if they are not used, hence why keyTyped is here
             public void keyTyped(KeyEvent e)
             {
             }
@@ -108,3 +188,8 @@ public class Map extends JPanel {
     }
 
 }
+//BUG: holding a key and then switching directions halts movement
+//how to reproduce: 
+// 1. hold any arrow key
+// 2. hold a second arrow key 
+// 3. release the second arrow key
